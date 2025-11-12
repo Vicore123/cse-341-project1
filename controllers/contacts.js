28384c274d6b28367e2f40f9ea54394afcb6f620
Vicore123 <viniciuslimaalves123@gmail.com>
2025-11-12 17:38:57 -0300

    W02 Project: Contacts Part 2

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,25 +1,106 @@
-const { response } = require('express');
-const mongodb = require('../data/database');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../data/database");
+const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db('cse-341-project1').collection('contacts').find()
-    result.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts);
-    });
-}
+  //#swagger.tags=['Contacts']
+  const result = await mongodb
+    .getDatabase()
+    .db("cse-341-project1")
+    .collection("contacts")
+    .find();
+  result.toArray().then((contacts) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(contacts);
+  });
+};
 
 const getByID = async (req, res) => {
-    const userId = new ObjectId(req.params.id)
-    const result = await mongodb.getDatabase().db('cse-341-project1').collection('contacts').find({_id: userId})
-    result.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0]);
+  //#swagger.tags=['Contacts']
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDatabase()
+    .db("cse-341-project1")
+    .collection("contacts")
+    .find({ _id: userId });
+  result.toArray().then((contacts) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(contacts[0]);
+  });
+};
+
+const createContact = async (req, res) => {
+  //#swagger.tags=['Contacts']
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+  .getDatabase()
+  .db("cse-341-project1")
+  .collection('contacts')
+  .insertOne(contact);
+
+  if (response.acknowledged) {
+    res.status(201).json({
+      message: 'Contact created successfully',
+      contactId: response.insertedId,
     });
+  }
+}
+
+const updateContact = async (req, res) => {
+  //#swagger.tags=['Contacts']
+  const contactId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+  .getDatabase()
+  .db("cse-341-project1")
+  .collection('contacts')
+  .replaceOne({ _id: contactId }, contact);
+
+  if (response.acknowledged) {
+    res.status(200).json({
+      message: 'Contact updated successfully',
+      modifiedCount: response.modifiedCount,
+    });
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the user.');
+  }
+}
+
+const deleteContact = async (req, res) => {
+  //#swagger.tags=['Contacts']
+  const contactId = new ObjectId(req.params.id);
+
+  const response = await mongodb
+  .getDatabase()
+  .db("cse-341-project1")
+  .collection('contacts')
+  .deleteOne({ _id: contactId });
+
+  if (response.acknowledged) {
+    res.status(200).json({
+      message: 'Contact deleted successfully',
+      deletedCount: response.deletedCount,
+    });
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the user.');
+  }
 }
 
 module.exports = {
-    getAll,
-    getByID
-}
+  getAll,
+  getByID,
+  createContact,
+  updateContact,
+  deleteContact
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const ContactsController = require("../controllers/contacts");
 
-router.get('/', ContactsController.getAll);
+router.get("/", ContactsController.getAll);
 
-router.get('/:id', ContactsController.getByID);
+router.get("/:id", ContactsController.getByID);
+
+router.post("/", ContactsController.createContact);
+
+router.put("/:id", ContactsController.updateContact);
+
+router.delete("/:id", ContactsController.deleteContact);
 
 module.exports = router;
